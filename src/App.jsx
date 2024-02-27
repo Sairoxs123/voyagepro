775f17b51bd7dbc579574172b524e96a12fef662
mxyxnk518 <140956050+mxyxnk518@users.noreply.github.com>
2024-02-27 23:41:25 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,25 @@ import { useState } from 'react';
 import { SignIn, SignUp, Hero, Navbar, Subscription, Footer, Generator } from './components';
 
 function App() {
-  const [theme, setTheme] = useState('cupcake');
-
-  const toggleTheme = () => {
-    setTheme(prevTheme => prevTheme === 'cupcake' ? 'luxury' : 'cupcake');
-  };
 
   return (
-    <div data-theme={theme} className='justify-center items-center'>
+    <div data-theme="luxury" className='justify-center items-center h-screen'>
       <Router>
         <Routes>
           <Route path="/signup" element={<SignUp />} />
           <Route path="/signin" element={<SignIn />} />
           <Route path="/" element={<>
-            <Navbar toggleTheme={toggleTheme} />
+            <Navbar />
             <Hero />
             <Subscription />
             <Footer />
           </>} />
-          <Route path='/generator' element={<Generator />} />
+          <Route path='/generator' element={<>
+          <div className=''>
+            <Navbar />
+            <Generator />
+          </div>
+          </>} />
         </Routes>
       </Router>
     </div>
