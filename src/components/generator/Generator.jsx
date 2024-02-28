f3b0a4a3b727aab3a5c9cc5b283e529ece7a560a
Sairoxs123 <127499321+Sairoxs123@users.noreply.github.com>
2024-02-28 17:27:30 +0400

    Update Generator.jsx

--- src/components/generator/Generator.jsx
+++ src/components/generator/Generator.jsx
@@ -91,14 +91,7 @@ const Generator = () => {
               />
             </div>
 
-            <div className="flex p-10" style={{ display: depDisplay }}>
-              <strong className="text-3xl m-5">Enter Date of Departure: </strong>
-              <input
-                type="date"
-                className="input input-ghost w-full max-w-xs m-5"
-                onChange={(e) => setDeparture(e.target.value)}
-              />
-            </div>      
+
 
             <br />
             <br />
