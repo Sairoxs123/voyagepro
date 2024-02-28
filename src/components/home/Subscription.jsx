42a683bf0b8205ffe8f17ffd8ddc18c1c7e83bdf
mxyxnk518 <140956050+mxyxnk518@users.noreply.github.com>
2024-02-28 12:13:44 +0400

    Update Subscription.jsx

--- src/components/home/Subscription.jsx
+++ src/components/home/Subscription.jsx
@@ -1,22 +1,18 @@
-import React from 'react'
+import React from 'react';
 
-const Subscription = ({theme}) => {
-
-
+const Subscription = () => {
   return (
-
-
-    <div className='flex justify-center items-center mt-20'>
-      <div className="group before:hover:scale-95 before:hover:h-72 before:hover:w-screen before:hover:rounded-b-2xl before:transition-all before:duration-500 before:content-[''] before:w-screen before:h-24 before:rounded-t-2xl before:bg-gradient-to-bl from-sky-400 via-orange-300 to-orange-600 before:absolute before:top-0 h-72 relative bg-slate-200 flex flex-col items-center justify-center gap-2 text-center rounded-2xl overflow-hidden w-screen">
-        <div className="w-28 h-28 bg-blue-600 mt-8 rounded-full border-4 border-slate-50 z-10 group-hover:scale-150 group-hover:-translate-x-24  group-hover:-translate-y-20 transition-all duration-500"></div>
-        <div className="z-10  group-hover:-translate-y-10 transition-all duration-500">
-          <span className="text-2xl font-semibold text-slate-800">Voyager Pro</span>
-          <p className="text-slate-800">Travel Smarter, Not Harder: Your Ultimate Itinerary Planner App!</p>
+    <div className='flex justify-center items-center'>
+      <div className="group before:hover:scale-95 before:hover:h-72 before:hover:w-screen before:hover:rounded-b-2xl before:transition-all before:duration-500 before:content-[''] before:w-screen before:h-24 before:rounded-t-2xl before:bg-gradient-to-bl from-yellow-800 via-cyan-700 to-yellow-600 before:absolute before:top-0 h-72 relative bg-black flex flex-col items-center justify-center gap-2 text-center rounded-2xl overflow-hidden w-screen">
+        <div className="w-28 h-28 bg-yellow-600 mt-8 rounded-full border-4 border-gray-400 z-10 group-hover:scale-150 group-hover:-translate-x-24 group-hover:-translate-y-20 transition-all duration-500"></div>
+        <div className="z-10 group-hover:-translate-y-10 transition-all duration-500">
+          <span className="text-2xl font-semibold text-gray-200">Voyager Pro</span>
+          <p className="text-gray-200">Travel Smarter, Not Harder: Your Ultimate Itinerary Planner App!</p>
         </div>
-        <a className="bg-blue-900 px-4 py-1 text-white rounded-md z-10 hover:scale-125 transition-all duration-500 hover:bg-blue-700" href="#">Subscribe</a>
+        <a className="bg-yellow-900 px-4 py-1 text-white rounded-md z-10 hover:scale-125 transition-all duration-500 hover:bg-yellow-700" href="#">Subscribe</a>
       </div>
     </div>
-  )
+  );
 }
 
-export default Subscription
+export default Subscription;
