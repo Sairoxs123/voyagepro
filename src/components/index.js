9fdf098113df38e0bfee410e871ff751ff338f5c
mxyxnk518 <140956050+mxyxnk518@users.noreply.github.com>
2024-02-28 21:30:13 +0400

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -2,20 +2,26 @@ import SignIn from "./authorisation/SignIn";
 import SignUp from "./authorisation/SignUp";
 import Hero from "./home/Hero";
 import Navbar from "./home/Navbar";
+import Selection from "./home/Selection";
 import Subscription from "./home/Subscription";
 import Footer from "./home/Footer";
 import Generator from "./generator/Generator";
 import Cards from "./home/Cards";
-import Home from "./subscribed/Home";
+import Faqs from "./home/Faqs";
+import Dashboard from "./dashboard/Dashboard";
 
 export {
+
     SignIn,
     SignUp,
     Navbar,
     Hero,
-    Generator,
+    Selection,
     Subscription,
     Footer,
+    Generator,
     Cards,
-    Home
+    Faqs,
+    Dashboard
+    
 }
