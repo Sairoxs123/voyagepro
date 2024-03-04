0a3286432704f5fea33193318de17a7ef3c188ca
Sairoxs123 <127499321+Sairoxs123@users.noreply.github.com>
2024-03-04 23:34:30 +0400

    Update Viewplan.jsx

--- src/components/dashboard/Viewplan.jsx
+++ src/components/dashboard/Viewplan.jsx
@@ -37,7 +37,7 @@ const Viewplan = () => {
   }, []);
 
   return (
-    plan !== "" ? <VerticalTimeline data={plan} /> : null
+    plan !== "" ? <VerticalTimeline data={plan} /> : <>hello</>
   );
 };
 
